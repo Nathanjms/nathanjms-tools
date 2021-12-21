941d088ac180285c5766d08c134724305974f1f2
Nathan James <nathanjms@outlook.com>
2021-12-21 17:46:05 +0000

    Add base styling from main site

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import './App.css';
+import { FaSpinner } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Nathan James.</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <FaSpinner className="spinner" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Coming Soon...
+        </p>
       </header>
-    </div>
+    </div >
   );
 }
 
