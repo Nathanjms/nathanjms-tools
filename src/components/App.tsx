2e48413b87e1ba4af7380d39cb8e59ced3ef7f5d
Nathan James <nathanjms@outlook.com>
2021-12-22 16:45:46 +0000

    Add footer to main page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "../css/App.css";
-import { FaSpinner } from 'react-icons/fa';
+import { FaSpinner } from "react-icons/fa";
+import Footer from "./global/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Nathan James.</h1>
-        <p>
-          <FaSpinner className="spinner" />
-        </p>
-        <p>
-          Coming Soon...
-        </p>
-      </header>
-    </div >
+    <React.Fragment>
+      <div className="wrapper">
+        <header className="App-header">
+          <h1>Nathan James.</h1>
+          <p>
+            <FaSpinner className="spinner" />
+          </p>
+          <p>Coming Soon...</p>
+        </header>
+      </div>
+      <Footer />
+    </React.Fragment>
   );
 }
 
