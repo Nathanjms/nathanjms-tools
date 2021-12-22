05dd8647e8ca73e24e44f5d3cca64188c26b6a90
Nathan James <nathanjms@outlook.com>
2021-12-22 16:44:55 +0000

    Add global components

---
+++ src/components/global/Footer.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { FaGithub, FaYoutube } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div id="footer">
+      <Container className="text-start">
+        <Row>
+          <Col md={6}>
+            <h4 style={{ paddingBottom: "20px" }}>Contact</h4>
+            <div className="contact">
+              <ul className="footerList list-unstyled">
+                <li>Nathan James</li>
+                <li style={{ paddingBottom: "5px" }}>nathan@nathanjms.co.uk</li>
+                <li>
+                  <a
+                    className="footerLogos"
+                    href="https://github.com/Nathanjms/"
+                  >
+                    <FaGithub />
+                  </a>
+                  <a
+                    className="footerLogos"
+                    href="https://www.youtube.com/channel/UCWL6DjV5c8oMBhOSzpvilmw"
+                  >
+                    <FaYoutube />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </Col>
+          <Col md={6}>
+            <h4 style={{ paddingBottom: "20px" }}>More</h4>
+            <div className="contact">
+              <ul className="footerList list-unstyled">
+                <li>
+                  <a href="https://www.nathanjms.co.uk">Go To Main Site</a>
+                </li>
+                <li>
+                  <a href="https://movies.nathanjms.co.uk">Movies</a>
+                </li>
+                <li>Last updated: 22nd Dec 2021</li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Footer;
