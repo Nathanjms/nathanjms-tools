f93c2e79ea45a69e0cf56e12417bad5dc0d35893
Nathan James <nathanjms@outlook.com>
2022-01-16 22:03:57 +0000

    Make active tab bolder

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,20 +1,40 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  const { pathname } = useLocation();
+  const isTabActive = (tabName: string): boolean => {
+    if (tabName === pathname.substring(1)) {
+      return true;
+    }
+    if (tabName === "unix-timestamp" && pathname === "/") {
+      return true;
+    }
+    return false;
+  };
   return (
-    <Navbar collapseOnSelect expand="lg" bg="grey" variant='dark' sticky="top">
+    <Navbar collapseOnSelect expand="lg" bg="grey" variant="dark" sticky="top">
       <Container>
         <Navbar.Brand href="/">Nathan James</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#unix-timestamp">Unix TimeStamp</Nav.Link>
-            <Nav.Link href="#character-count">Character Count</Nav.Link>
+            <Nav.Link active={isTabActive("unix-timestamp")} href="/">
+              Unix Timestamp
+            </Nav.Link>
+            <Nav.Link
+              active={isTabActive("character-count")}
+              href="character-count"
+            >
+              Character Count
+            </Nav.Link>
             <Nav.Item className="text-muted">Map [WIP]</Nav.Item>
           </Nav>
           <Nav>
-            <Nav.Link href="https://nathanjms.co.uk">Go to <em>nathanjms.co.uk</em></Nav.Link>
+            <Nav.Link href="https://nathanjms.co.uk">
+              Go to <em>nathanjms.co.uk</em>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
