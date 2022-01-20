4405b12913519edd25c93ae731bd167dcaa7e263
Nathan James <nathanjms@outlook.com>
2022-01-20 18:27:26 +0000

    Make router go to default and pass the component as a prop

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,28 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../css/App.css";
-import Footer from "./global/Footer";
-import Header from "./global/Header";
 import NotFound from "./global/NotFound";
-import Home from "./main/Home";
 import UnixTime from "./main/unixtime/UnixTime";
 import CharacterCount from "./main/character-count/CharacterCount";
 import ColumnToCsv from "./main/column-to-csv/ColumnToCsv";
+import Default from "./global/Default";
 
 function App() {
   return (
     <React.Fragment>
       <Router>
-        <Header />
-        <div className="wrapper container">
-          <Home />
-          <Routes>
-            <Route path="/" element={<UnixTime />} />
-            <Route path="/character-count" element={<CharacterCount />} />
-            <Route path="/column-to-csv" element={<ColumnToCsv />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-        <Footer />
+        <Routes>
+          <Route path="/" element={<Default component={<UnixTime />} />} />
+          <Route
+            path="/character-count"
+            element={<Default component={<CharacterCount />} />}
+          />
+          <Route
+            path="/column-to-csv"
+            element={<Default component={<ColumnToCsv />} />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Router>
     </React.Fragment>
   );
