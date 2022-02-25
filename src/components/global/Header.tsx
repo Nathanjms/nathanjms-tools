a53c79173b91a93f7b8f1f11c9b0b4fb898ee262
Nathan James <nathanjms@outlook.com>
2022-02-25 18:46:27 +0000

    Make use of react router's `NavLink` instead, keep dependencies lower

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import headerItems from "../../data/headerItems.json";
-import { LinkContainer } from "react-router-bootstrap";
 
 export default function Header() {
-  const { pathname } = useLocation();
-  const isTabActive = (tabName: string, homePage: boolean): boolean => {
-    if (homePage && pathname === "/") {
-      return true; // Set as homepage
-    }
-    if (tabName === pathname.substring(1)) {
-      return true;
-    }
-    return false;
-  };
   return (
     <Navbar collapseOnSelect expand="lg" bg="grey" variant="dark" sticky="top">
       <Container>
@@ -23,14 +12,13 @@ export default function Header() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {headerItems.map((element) => (
-              <LinkContainer to={element.link}>
-                <Nav.Link
-                  key={element.id}
-                  active={isTabActive(element.tabName, element.homePage)}
-                >
-                  {element.name}
-                </Nav.Link>
-              </LinkContainer>
+              <NavLink
+                className="nav-link"
+                to={element.link}
+                key={element.id}
+              >
+                {element.name}
+              </NavLink>
             ))}
           </Nav>
           <Nav>
