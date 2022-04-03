ccf2a15d0308abe8cfd3138ccfb30027ab1c1892
Nathan James <nathanjms@outlook.com>
2022-04-03 16:37:40 +0100

    regex

--- src/components/main/unixtime/UnixTime.tsx
+++ src/components/main/unixtime/UnixTime.tsx
@@ -272,4 +272,7 @@ export const SwalErrorNaNMessage = (errMessage: string = "Error"): void => {
   });
 };
 
+const validSqlDate =
+  /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$/;
+
 export default UnixTime;
